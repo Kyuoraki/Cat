39aa43f5045c52bb88b47b8c9aa8e7285eab1b40
”Buharin <black_cloak56@mail.ru>
2021-02-08 23:37:34 +0500

    ura

---
+++ src/Loader.java
@@ -0,0 +1,30 @@
+
+public class Loader {
+    public static void main(String[] args) {
+        Cat Vasya = new Cat();
+        Cat Musya = new Cat();
+        Cat Kot = new Cat();
+        Cat Pidor = new Cat();
+        Cat Makimus = new Cat();
+        Cat Oleg = new Cat();
+
+        System.out.println("Vasya: " + Vasya.getWeight());
+        System.out.println("Musya: " + Musya.getWeight());
+        System.out.println("Kot: " + Kot.getWeight());
+        System.out.println("Pidor: " + Pidor.getWeight());
+        System.out.println("Makimus: " + Makimus.getWeight());
+        System.out.println("Oleg: " + Oleg.getWeight());
+
+//        while (Makimus.getWeight() >= 1000) {
+//            Makimus.meow();
+//        }
+//        System.out.println("Makimus: " + Makimus.getStatus());
+//        System.out.println("Makimus: " + Makimus.getWeight());
+
+        while (Makimus.getWeight() <= 9000) {
+            Makimus.feed((double) 1);
+        }
+        System.out.println(Makimus.getStatus());
+
+    }
+}
