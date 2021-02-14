d1d36e80a3a51302bb1f06a93b4cb4a7f50d14f0
”Buharin <black_cloak56@mail.ru>
2021-02-15 02:12:25 +0500

    you can't feed dead cat

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,8 @@
 
-public class Cat
-{
+public class Cat {
+    private static int count;
+    private static String status;
+
     private double originWeight;
     private double weight;
 
@@ -9,59 +11,70 @@ public class Cat
 
     private double eatable;
 
-    public Cat()
-    {
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        count++;
 
     }
-
-    public void meow()
-    {
-        weight = weight - 1;
-        System.out.println("Meow");
+    public void meow() {
+        if (weight > maxWeight || weight < minWeight) {
+            System.out.println("i can't meow");
+        } else {
+            weight = weight - 1;
+            System.out.println("Meow");
+        }
     }
-
-    public void feed(Double amount)
-    {
-        weight = weight + amount;
+    public void feed(Double amount) {
+        if (weight > maxWeight || weight < minWeight) {
+            System.out.println("i can't eat");
+        } else {
+            weight = weight + amount;
+        }
     }
-
-    public void drink(Double amount)
-    {
-        weight = weight + amount;
+    public void drink(Double amount) {
+        if (weight > maxWeight || weight < minWeight) {
+            System.out.println("i can't drink");
+        } else {
+            weight = weight + amount;
+        }
     }
-
-    public void pee(Double amount)
-    {
-        weight = weight - amount;
-        System.out.println("пись-пись");
+    public void pee(Double amount) {
+        if (weight > maxWeight || weight < minWeight) {
+            System.out.println("i can't pee-pee");
+        } else {
+            weight = weight - amount;
+            System.out.println("пись-пись");
+        }
     }
-
-    public Double getWeight()
-    {
+    public Double getWeight() {
         return weight;
     }
-
-    public String getStatus()
-    {
-        if(weight < minWeight) {
+    public String getStatus() {
+        if (weight < minWeight) {
+            count--;
             return "Dead";
-        }
-        else if(weight > maxWeight) {
+        } else if (weight > maxWeight) {
+            count--;
             return "Exploded";
-        }
-        else if(weight > originWeight) {
+        } else if (weight > originWeight) {
             return "Sleeping";
-        }
-        else {
+        } else {
             return "Playing";
         }
     }
-    public Double getEatable(){
-        eatable = weight - originWeight;
+    public static int getCount() {
+        return count;
+    }
+    public Double getEatable() {
+        if (weight > maxWeight || weight < minWeight) {
+            System.out.println(" don't matter at all!");
+        } else {
+            eatable = weight - originWeight;
+//            System.out.println(eatable);
+        }
         return eatable;
     }
 }
--- src/Loader.java
+++ src/Loader.java
@@ -9,24 +9,32 @@ public class Loader {
         Cat Oleg = new Cat();
 
         System.out.println("Vasya: " + Vasya.getWeight());
-        Vasya.feed((double) 10);
-        System.out.println(Vasya.getEatable());
-        System.out.println("Vasya: " + Vasya.getWeight());
-        Vasya.pee((double) 10);
-        System.out.println("Vasya: " + Vasya.getWeight());
+        System.out.println("Кол-во котов: " + Cat.getCount());
 
-//        System.out.println("Musya: " + Musya.getWeight());
-//        System.out.println("Kot: " + Kot.getWeight());
-//        System.out.println("Pidor: " + Pidor.getWeight());
-//        System.out.println("Makimus: " + Makimus.getWeight());
-//        System.out.println("Oleg: " + Oleg.getWeight());
+        System.out.println("Musya: " + Musya.getWeight());
+        System.out.println("Kot: " + Kot.getWeight());
+        System.out.println("Pidor: " + Pidor.getWeight());
+        System.out.println("Makimus: " + Makimus.getWeight());
+        Oleg.pee((double) 10000);
+        System.out.println("Oleg: " + Oleg.getStatus());
+        Pidor.feed((double) 10000);
+        System.out.println("Pidor: " + Pidor.getStatus());
+        Oleg.pee((double) 1);
+        Pidor.feed((double) 1);
+        Oleg.meow();
+        Pidor.drink((double) 1);
+        System.out.println("Сколько съел%: " + Oleg.getEatable());
+        Oleg.getEatable();
+        System.out.println("Кол-во котов: " + Cat.getCount());
+
+
 
 //        while (Makimus.getWeight() >= 1000) {
 //            Makimus.meow();
 //        }
 //        System.out.println("Makimus: " + Makimus.getStatus());
 //        System.out.println("Makimus: " + Makimus.getWeight());
-
+//
 //        while (Makimus.getWeight() <= 9000) {
 //            Makimus.feed((double) 1);
 //        }
