b19bfedd2320511d905a688a70e8db39d401162d
”Buharin <black_cloak56@mail.ru>
2021-02-20 23:43:21 +0500

    клонирование объекта

--- src/Loader.java
+++ src/Loader.java
@@ -1,18 +1,6 @@
 
 public class Loader {
     public static void main(String[] args) {
-//        Cat Oleg = new Cat(0.0);
-//        Cat Kurlik = new Cat(0.0);
-//        Cat Vadik = new Cat(0.0);
-
-//        Oleg.setColor(Color.BLACK);
-//        Kurlik.setColor(Color.GREY);
-//        Vadik.setColor(Color.TRIPLE_COLOR);
-//
-//        System.out.println("Oleg: " + Oleg.getColor());
-//        System.out.println("Kurlik: " + Kurlik.getColor());
-//        System.out.println("Vadik: " + Vadik.getColor());
-
         Cat murka = new Cat("Murka", 3000.00);
         System.out.println(murka.getWeight());
         System.out.println(murka.getStatus());
